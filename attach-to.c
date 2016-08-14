bc4e785ac53e6fdd5c4beaea91b01a9565d328bb
k-stz@thalassa <keistzen@gmail.com>
2016-08-15 00:02:10 +0200

    makefile update

---
+++ attach-to.c
@@ -0,0 +1,29 @@
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+
+// ptrace prototype/signature:
+// #include <sys/ptrace.h>
+// long int ptrace(enum __ptrace_request request, pid_t pid,
+//                 void *addr, void *data)
+
+
+pid_t target_process;
+
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("Usage: ./attach-to.c <pid-of-target-process>\n");
+  }
+  printf("WERKS\n");
+
+}
+
+
--- makefile
+++ makefile
@@ -2,7 +2,13 @@ ptrace-test: ptrace-test.c
 build:
 	gcc -o bin/ptrace-test ptrace-test.c
 	gcc -o bin/return-x return-x.c
+	gcc -o bin/attach-to attach-to.c
 
-run:
+run-ptrace-test:
 	./bin/ptrace-test ./bin/return-x
 
+run-return-x:
+	./bin/return-x
+
+run-attach-to:
+	./bin/attach-to
