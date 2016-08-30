318366c0013187739de57d7df3ac9a69bcf1679e
k-stz@thalassa <keistzen@gmail.com>
2016-08-30 20:32:42 +0200

    gcc -g notes

--- spam.c
+++ spam.c
@@ -1,17 +1,17 @@
-// print some spam - used to test singlestepping for example
+/* print some spam - used to test singlestepping for example */
 
 #include <stdio.h>
 
 char string[] = "Find this String in memory";
 
 int x = 0;
-int flagg = 43981; // Hex representation: abcd
+int flagg = 43981; /* Hex representation: abcd */
 
 int main(int argc, char **argv) {
-  // flagg will be found in the RAX register!
+  /* flagg will be found in the RAX register! */
   while(flagg == 43981) { 
-    // TODO: it seems RAX alters between containing "flagg" and x. find a way to display
-    // the instructions executed
+    /* TODO: it seems RAX alters between containing "flagg" and x. find a way to display */
+    /* the instructions executed */
     x++;
   }
   return x;
