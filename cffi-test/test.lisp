939ffea946eea46893add30c74d83eac62667cdc
k-stz@sao <keistzen@gmail.com>
2017-01-18 09:15:36 +0100

    cffi creating binds notes; notes on .a .so files

--- cffi-test/test.lisp
+++ cffi-test/test.lisp
@@ -27,15 +27,9 @@
 	  "/usr/lib/x86_64-linux-gnu/libc.so"
 	  "libc.so.6" "libc.so" "libc-2.24.so" "libc.a"
 	  "/usr/lib/libc-2.24.so" "/usr/lib/libc-2.24"))
-  ;; TODO: how to actually load differnt versions with (:user (:OR ..)) ??
-  ;; ubuntu:
     (t (:default "/lib/x86_64-linux-gnu/libc-2.19"
 	   "/usr/lib/libc.so")))
 
-
-;; NEXT-TODO:
-;; from here on we might just need to work with the header files
-;; sys/ptrace.h
 
 ;; this effectively loads libc into the Lisp image much, just like we load .lisp files
 ;; into the lisp image. "much like the linker does _when you start_ a C program
